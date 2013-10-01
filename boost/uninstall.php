e7bec87f949f944299a0cf99721120192c8e7ac7
Mitch Geer <mitch@blackfoot.appstate.edu>
2013-10-01 12:34:54 -0400

    Changed getTable to buildTable.

--- boost/uninstall.php
+++ boost/uninstall.php
@@ -11,9 +11,9 @@ function always_uninstall(&$content)
     $db = Database::newDB();
     
     if ($db->tableExists('always_student')) {
-        $tbl = $db->getTable('always_student');
+        $tbl = $db->buildTable('always_student');
         $tbl->drop();
-    }
+   }
 
     return true;
 }
